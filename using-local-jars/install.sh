45116fcfa254385327787a55d4a8df7857f484b3
liumapp <liumapp.com@gmail.com>
2018-04-07 18:47:43 +0800

    test install shell

--- using-local-jars/install.sh
+++ using-local-jars/install.sh
@@ -12,3 +12,11 @@ echo '$                                                           $'
 echo '============================================================='
 echo '.'
 
+mvn -v
+
+$PATH=/usr/local/tomcat/project/maven-deal-multy-local-jar/using-local-jars
+
+mvn install:install-file -Dfile=commons-io-2.5.jar -DgroupId=org.apache.commons.io \
+                         -DartifactId=commons-io -Dversion=2.5 \
+                         -DlocalRepositoryPath=${PATH}/lib/commons-io-2.5.jar
+
