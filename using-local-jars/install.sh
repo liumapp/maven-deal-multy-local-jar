f326bcc5b93949df0a1e7c65508a526e173253f3
liumapp <liumapp.com@gmail.com>
2018-04-07 19:21:18 +0800

    use localM2 to represent maven local home

--- using-local-jars/install.sh
+++ using-local-jars/install.sh
@@ -14,15 +14,17 @@ echo '.'
 
 mvn -v
 
+export localM2=/Users/liumeishengqi/.m2/repository
+
 mvn install:install-file -Dfile=lib/commons-io-2.5.jar -DgroupId=org.apache.commons.io \
                          -DartifactId=commons-io -Dversion=2.5 \
-                         -Dpackaging=jar -DlocalRepositoryPath=/Users/liumeishengqi/.m2/repository
+                         -Dpackaging=jar -DlocalRepositoryPath=${localM2}
 
 mvn install:install-file -Dfile=lib/simple-jar-a-v1.0.0.jar -DgroupId=com.liumapp.simple \
                          -DartifactId=simple-jar-a -Dversion=v1.0.0 \
-                         -Dpackaging=jar -DlocalRepositoryPath=/Users/liumeishengqi/.m2/repository
+                         -Dpackaging=jar -DlocalRepositoryPath=${localM2}
 
 mvn install:install-file -Dfile=lib/simple-jar-b-v1.0.0.jar -DgroupId=com.liumapp.simple \
                          -DartifactId=simple-jar-b -Dversion=v1.0.0 \
-                         -Dpackaging=jar -DlocalRepositoryPath=/Users/liumeishengqi/.m2/repository
+                         -Dpackaging=jar -DlocalRepositoryPath=${localM2}
 
