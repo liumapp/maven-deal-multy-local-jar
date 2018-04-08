195d9f636f4caa786841cff7e6b3c9cf3630acce
liumapp <liumapp.com@gmail.com>
2018-04-08 09:18:33 +0800

    add auto generate pom file

--- using-local-jars/install.sh
+++ using-local-jars/install.sh
@@ -14,21 +14,21 @@ echo '.'
 
 mvn -v
 
-export localM2=/Users/liumeishengqi/.m2/repository
-#export localM2=/usr/local/tomcat/project/maven-deal-multy-local-jar/using-local-jars/repo
+#export localM2=/Users/liumeishengqi/.m2/repository
+export localM2=/usr/local/tomcat/project/maven-deal-multy-local-jar/using-local-jars/repo
 
 mvn install:install-file -Dfile=lib/commons-io-2.5.jar -DgroupId=org.apache.commons.io \
                          -DartifactId=commons-io -Dversion=2.5 \
                          -Dpackaging=jar -DlocalRepositoryPath=${localM2} \
-                         -DupdateReleaseInfo=true
+                         -DgeneratedPom=true
 
 mvn install:install-file -Dfile=lib/simple-jar-a-v1.0.0.jar -DgroupId=com.liumapp.simple \
                          -DartifactId=simple-jar-a -Dversion=v1.0.0 \
                          -Dpackaging=jar -DlocalRepositoryPath=${localM2} \
-                         -DupdateReleaseInfo=true
+                         -DgeneratedPom=true
 
 mvn install:install-file -Dfile=lib/simple-jar-b-v1.0.0.jar -DgroupId=com.liumapp.simple \
                          -DartifactId=simple-jar-b -Dversion=v1.0.0 \
                          -Dpackaging=jar -DlocalRepositoryPath=${localM2} \
-                         -DupdateReleaseInfo=true
+                         -DgeneratedPom=true
 
